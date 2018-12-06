2d2733b1b2cbd9dfe6eb9551b740c15a1865d912
lezhnina-anna <Ankemova@gmail.com>
2018-12-06 23:30:22 +0300

    update styles

--- src/home-page/HomePage.jsx
+++ src/home-page/HomePage.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Component} from "react";
 import {Link} from 'react-router-dom';
-import "./HomePageStyle.css";
+import "./HomePageStyle.scss";
 import {observer} from "mobx-react";
 import autobind from "autobind-decorator";
 
@@ -15,27 +15,13 @@ class HomePage extends Component {
 
     render() {
         return (
-            <div>
-                <div className={"authorization"}>
-                    {!localStorage.getItem("token") ?
-                        <>
-                            <Link className={"btn btn-primary"} id={"signin"} to={"/signin"}>Sign In</Link>
-                            <Link className={"btn btn-primary"} id={"signup"} to={"/signup"}>Sign Up</Link>
-                        </>
-                        :
-                        <>
-                            <Link className={"btn btn-primary"} id={"profile-button"} to={"profile"}>Profile</Link>
-                            <button className={"btn btn-primary"} id={"logout"} onClick={this.handleLogOut}>Log Out
-                            </button>
-                        </>
-                    }
-                </div>
+            <div className={"home-page"}>
                 <h1 className={"site-name"}>Order task</h1>
                 <div className={"about-us"}>
                     <p>Some info...</p>
                 </div>
                 <p className={"button-order"}>
-                    <Link className={"btn btn-primary"} id={"create-project"} to={"/signup"}>Create order</Link>
+                    <Link className={"btn btn-info"} id={"create-project"} to={"/projectList"}>View Projects</Link>
                 </p>
             </div>
         );
---
+++ src/layout.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import autobind from "autobind-decorator";
+import {Link} from 'react-router-dom';
+import "./layout.scss";
+
+@autobind
+class layout extends React.Component {
+    children;
+
+    constructor(component) {
+        super();
+        this.children = component;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={"header"}>
+                    <Link className={"header-link home"} to={"/"}>Home Page</Link>
+                    {!localStorage.getItem("token") ?
+                        <>
+                            <Link className={"header-link"} id={"signin"} to={"/signin"}>Sign In</Link>
+                            <Link className={"header-link"} id={"signup"} to={"/signup"}>Sign Up</Link>
+                        </>
+                        :
+                        <>
+                            <Link className={"header-link"} to={"profile"}>Profile</Link>
+                            <span className={"logout"} onClick={this.handleLogOut}>Log Out</span>
+                        </>
+                    }
+                </div>
+                {this.children}
+            </div>
+        )
+    }
+}
+
+export {layout};

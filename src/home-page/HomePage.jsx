806b454539154873fc9f21c6075eaa8290f13702
fjodor-rybakov <fedryb@mail.ru>
2019-01-06 21:27:03 +0300

    add req popular

--- src/home-page/HomePage.jsx
+++ src/home-page/HomePage.jsx
@@ -5,10 +5,25 @@ import "./HomePageStyle.scss";
 import {observer} from "mobx-react";
 import {autobind} from "core-decorators";
 import {Header} from "../header/Header";
+import * as rp from "request-promise";
 
 @observer
 @autobind
 class HomePage extends Component {
+    componentDidMount() {
+        const options = {
+            method: "GET",
+            url: `${localStorage.getItem("serverAddress")}/api/project/getMostPopular`,
+            headers: {
+                "Cache-Control": "private, max-age=0, no-cache"
+            },
+        };
+        rp(options)
+            .then(JSON.parse)
+            .then(console.log)
+            .catch(console.log);
+    }
+
     render() {
         return (
             <div className={"home-page"}>

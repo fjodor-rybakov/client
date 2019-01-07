091cd26ebe95c94ae3b96eaa6922fca2c7be54fb
fjodor-rybakov <fedryb@mail.ru>
2019-01-07 19:16:55 +0300

    fix request

--- src/project/components/AddTaskForm/AddTaskFormController.js
+++ src/project/components/AddTaskForm/AddTaskFormController.js
@@ -4,7 +4,6 @@ import * as rp from "request-promise";
 
 @autobind
 class AddTaskFormController extends Component {
-
     async addTask(data) {
         const options = {
             method: "POST",
@@ -13,7 +12,7 @@ class AddTaskFormController extends Component {
                 "x-guide-key": localStorage.getItem("token"),
                 "Cache-Control": "private, max-age=0, no-cache"
             },
-            body: {data: data},
+            body: data,
             json: true
         };
         return rp(options);
